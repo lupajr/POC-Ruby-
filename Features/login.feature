39f85fed4cede86587ca91b40f5ebd68603c35d6
Automação <luiz.martins@clinicorp.com>
2023-12-08 17:35:49 -0300

    adição de Rspec e adição de testes para exemplo

--- Features/login.feature
+++ Features/login.feature
@@ -1,7 +1,14 @@
 Feature: Login
-  Scenario: Successful login
-    Given I am on the login page
-    When I fill in "username" with "your_username"
-    And I fill in "password" with "your_password"
-    And I click the "Login" button
-    Then I should see "Welcome, your_username!"
+
+  #Scenario: Successful login
+    #Given I am on the login page
+    #When I fill in "username" with "your_username"
+    #And I fill in "password" with "your_password"
+    #And I click the "Login" button
+    #Then I should see "Welcome, your_username!"
+
+  Scenario: cenario do edu
+    Given que estou na tela da clinicorp
+    When clico no botao de assine agora
+    And e clico plano std
+    Then vejo a tela de login
--- Features/step_definitions/login_steps.rb
+++ Features/step_definitions/login_steps.rb
@@ -1,3 +1,6 @@
+require "capybara/rspec"
+
+
 Given('I am on the login page') do
   visit 'https://www.google.com'
 end
@@ -12,4 +15,39 @@ end
 
 Then('I should see {string}') do |string|
 
+end
+
+#-------------------------------------
+Given('que estou na tela da clinicorp') do
+    visit ("https://www.clinicorp.com/")
+end
+
+When('clico no botao de assine agora') do
+  first('span', text: 'Assine agora').click
+end
+
+When('e clico plano std') do
+  find('span', text: 'Assinar Standard').click
+end
+
+Then('vejo a tela de login') do
+  find_by_id('fullName').set ("Lucas lindo e gostoso.mp3")
+  find_by_id('clinicName').set ("Lucas lindo e gostoso.mp3")
+  find_by_id('documentNumber').set ("43499284804")
+  find_by_id('email').set ("lucas.gambarino@clinicorp.com")
+  find_by_id('personalPhone').set ("45984318619")
+  find_by_id('businessPhone').set ("38575438")
+  find_by_id('cep').set ("89255726")
+  find('span', text: 'Buscar Endereço').click
+  find_by_id('number').set ("123")
+  find_by_id('address_complement').set ("casa 1")
+  
+  @validation_id = 'businessPhone'
+
+  if find_by_id("@validation_id" == businessPhone )
+    puts "teste validado"
+  else
+    fail   
+  end
+  sleep 30
 end
